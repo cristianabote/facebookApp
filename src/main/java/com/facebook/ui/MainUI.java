bc745bbd1d31e7dabd135f9f11de8964c39626a1
soundrob <paltanrobert@yahoo.com>
2020-02-11 07:27:04 +0200

    Sign up almoast ready

--- src/main/java/com/facebook/ui/MainUI.java
+++ src/main/java/com/facebook/ui/MainUI.java
@@ -2,14 +2,39 @@ package com.facebook.ui;
 
 import sun.rmi.runtime.Log;
 
+import java.util.Scanner;
+
 public class MainUI {
 
-    private LoginUI loginUI = new LoginUI();
-
-//    private SignUpUI signUpUI
+//    private LoginUI loginUI = new LoginUI();
+//    private RegisterUI registerUI = new RegisterUI();
 
     public void start() {
-        loginUI.displayLogin();
-    }
 
+        Scanner loginScanner = new Scanner(System.in);
+
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("Enter your option: ");
+
+        int option = Integer.parseInt(loginScanner.next());
+
+
+        if (option == 1) {
+            System.out.println("Please Login ");
+            LoginUI loginUI = new LoginUI();
+            loginUI.displayLogin();
+        }
+        if (option == 2) {
+            {
+                System.out.println("Please Register ");
+                RegisterUI registerUI = new RegisterUI();
+                registerUI.displayRegister();
+            }
+
+
+
+        }
+
+    }
 }
